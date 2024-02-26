34ab881e2acd9a3547c557d3455fa756b41a0c2e
Hrithik MJ <31767239+HrithikMJ@users.noreply.github.com>
2024-02-26 22:42:42 +0530

    Update: Added Results

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,62 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-import { useEffect, useMemo, useRef } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useRef, useState, useCallback } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [init, setInit] = useState(true);
   const paragraphRef = useRef("");
   const keywordsRef = useRef("");
-
-  // useEffect(() => {
-  //   initParticlesEngine(async (engine) => {
-  //     // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-  //     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-  //     // starting from v2 you can add only the features you need reducing the bundle size
-  //     await loadAll(engine);
-  //     // await loadFull(engine);
-  //     // await loadSlim(engine);
-  //   }).then(() => {
-  //     setInit(true);
-  //   });
-  // }, []);
-
-  const options = useMemo(
-    () => ({
-      particles: {
-        number: {
-          value: 10,
-        },
-        color: {
-          value: "#ffffff",
-        },
-        links: {
-          enable: true,
-          distance: 200,
-        },
-        shape: {
-          type: "circle",
-        },
-        opacity: {
-          value: 1,
-        },
-        size: {
-          value: {
-            min: 4,
-            max: 6,
-          },
-        },
-        move: {
-          enable: true,
-          speed: 2,
-        },
-      },
-    }),
-    []
-  );
-  // const particlesLoaded = (container) => {
-  //   console.log(container);
-  // };
+  const [jsonResult,setJsonResult]=useState({})
   const handleSubmission = (e) => {
-    e.preventDefault()
-    
-    var paragraph=paragraphRef.current?.value
-    var keywords =keywordsRef.current?.value
-    console.log(paragraph,keywords)
+    e.preventDefault();
+    var paragraph = paragraphRef.current?.value;
+    var keywords = keywordsRef.current?.value;
+    console.log(paragraph, keywords);
     // keywordsRef.current.value=""
-    if (paragraph.length > 0 && keywords.length>0 && paragraph.trim().split(' ').length >= 100) {
+    if (
+      paragraph.length > 0 &&
+      keywords.length > 0 &&
+      paragraph.trim().split(" ").length >= 100
+    ) {
       fetch("http://localhost:5000/postResults", {
-        method: 'post',
+        method: "post",
         headers: {
-          "Access-Control-Allow-Origin":"*",'Content-Type':'application/json',
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          "paragraph":paragraph,
-          "keywords":keywords,
+          paragraph: paragraph,
+          keywords: keywords,
+        }),
+      })
+        .then((res) => {
+          res.json().then((w) => {
+            console.log(w)
+            setJsonResult(JSON.parse(w))
+            setInit(false)
+
+          })
         })
-       }).then((res)=>{
-        console.log(res)
-       });  
-    }
-    else{
-      console.error("Please veify that the paragraph and keywords are valid.")
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      console.error("Please verify that the paragraph and keywords are valid.");
+      window.alert("Invalid Paragraph/Keyword");
     }
   };
   if (init) {
     return (
       <div className="Home">
-        {/* <Particles
-            id="tsparticles"
-            particlesLoaded={particlesLoaded}
-            options={options}
-          /> */}
         <div className="homeSection">
           <form onSubmit={handleSubmission}>
-            <input
+            <textarea
               id="para"
               ref={paragraphRef}
               name="paragraph"
-              placeholder="Enter Summary Here"
-              type="text"
+              placeholder="Enter Summary Of Minimum 100 words Here"
+              type="textArea"
             />
             <input
               id="keyWords"
@@ -110,13 +65,29 @@ function App() {
               placeholder="Enter Key Words"
               type="text"
             />
-            <input type="submit" value="submit" />
+
+            <input id="submit" type="submit" value="Submit" />
           </form>
         </div>
       </div>
     );
   }
-  return <></>;
+  return <div className="Home">
+  <div className="homeSection">
+    <div className="homeContainer">
+    <h1>
+      <span>Grammar Score:</span> {jsonResult.result.grammar}%
+    </h1>
+    <h1>
+      <span>Relevance Score:</span> {jsonResult.result.relevance}%
+    </h1>
+    <h1 id="fb">
+    <span>Feedback:</span> {jsonResult.result.feedback}
+    </h1>
+    <button onClick={()=>{location.reload()}}>Go Back</button>
+    </div>
+  </div>
+</div>;
 }
 
 export default App;
